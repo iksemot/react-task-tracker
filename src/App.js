7539f26be18bce3e7af8177364e16c15b81cc409
Tomasz Rydzewski <tomasz.rydzewski@outlook.com>
2018-01-18 17:20:59 +0100

    Move tasks to an array of tasks

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import TaskItem from './task-item'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      tasks: [
+        {description: 'First thing', isDone: false },
+        {description: 'Second thing', isDone: false },
+        {description: 'Thids thing', isDone: true }
+      ]
+    }
+
+  }
+
   render() {
     return (
       <div>
         <input type="text" />
         <h3>TODO</h3>
         <div>
-          <TaskItem description="First thing" isDone={false} />
-          <TaskItem description="Second thing" isDone={false} />
+          {this.state.tasks.map(task => <TaskItem description={task.description} isDone={task.isDone} />)}
         </div>
         <h3>DONE</h3>
         <div>
-          <TaskItem description="Third thing done" isDone={true} />
+          {this.state.tasks.map(task => <TaskItem description={task.description} isDone={task.isDone} />)}
         </div>
       </div>
     );
